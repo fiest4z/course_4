35c2c25b4f1b1b052f7ed8f0d47399cbf336764b
Mikhail Belyaev <fiestaz1337@gmail.com>
2024-07-26 22:56:38 +0300

    upd utils

--- src/utils.py
+++ src/utils.py
@@ -53,3 +53,39 @@ def formatting_vacancies_to_list(keyword):
             )
             vacancies.append(vacancy)
     return vacancies
+
+
+def user_interaction():
+    json_file = JSONFile('data/vacancies.json')
+    platforms = ['HeadHunter']
+
+    search_query = input('Введите поисковый запрос: ')
+    vacancies_list = formatting_vacancies_to_list(search_query)
+    filter_words = str(input('Введите ключевые слова для фильтрации вакансий по описанию: ').split())
+    filtered_vacancies = filtering_vacancies(vacancies_list, filter_words)
+
+    if not filtered_vacancies:
+        print('Вакансии не найдены')
+        return None
+
+    print("Найдены вакансии по запросу: \n")
+    for idx, vacancy in enumerate(filtered_vacancies, start=1):
+        print(f'{idx}. {vacancy}')
+        json_file.add_data_to_dict(vacancy)
+
+    top_n = int(input('Введите количество вакансий для вывода: '))
+    vacancies_with_salary = [vacancy for vacancy in filtered_vacancies if vacancy.salary_min is not None
+                             or vacancy.salary_max is not None]
+    ranged_vacancies = sorted(vacancies_with_salary, key=lambda x: x.salary_min if x.salary_min is not None else float(
+        'inf'), reverse=True)
+    print(f'\nТоп-{top_n} вакансий по зарплате: \n')
+    for idx, vacancy in enumerate(ranged_vacancies[:top_n], start=1):
+        print(f'{idx}. {vacancy}')
+
+    clean_data = input("Очистить список? y/n: ")
+    if clean_data.lower() == 'y':
+        json_file.del_data_dict()
+
+    run_again = input("Повторить поиск? y/n: ")
+    if run_again.lower() == 'y':
+        user_interaction()
